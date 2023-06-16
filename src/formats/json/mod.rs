9781cd6a4cfa4573a78bd2e691cb6f29718ad18d
Xuanwo <github@xuanwo.io>
2023-06-16 10:31:06 +0800

    feat: Implement parse from sort_order (#10)
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/formats/json/mod.rs
+++ src/formats/json/mod.rs
@@ -1,7 +1,9 @@
 mod schema_v2;
 pub use schema_v2::parse_schema_v2;
-mod partition;
-pub use partition::parse_partition_spec;
+mod partition_spec;
+pub use partition_spec::parse_partition_spec;
+mod sort_order;
+pub use sort_order::parse_sort_order;
 
 mod transform;
 mod types;
