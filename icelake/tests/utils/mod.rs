5a4fc46ca309ce565f254a2543e0e0f47a8be697
Renjie Liu <liurenjie2008@gmail.com>
2023-08-28 15:17:21 +0800

    Update poetry

--- icelake/tests/utils/mod.rs
+++ icelake/tests/utils/mod.rs
@@ -1,8 +1,11 @@
 use testcontainers::{Container, GenericImage};
 
 mod poetry;
+
 pub use poetry::*;
+
 mod containers;
+
 pub use containers::*;
 
 use arrow::csv::ReaderBuilder;
@@ -32,18 +35,6 @@ pub fn run_command(mut cmd: Command, desc: impl ToString) {
         log::info!("{} succeed!", desc)
     } else {
         panic!("{} failed: {:?}", desc, exit);
-    }
-}
-
-/// Same function as `run_command`, but will not panic if run fail.
-pub fn run_command_quiet(mut cmd: Command, desc: impl ToString) {
-    let desc = desc.to_string();
-    log::info!("Starting to {}, command: {:?}", &desc, cmd);
-    let exit = cmd.status().unwrap();
-    if exit.success() {
-        log::info!("{} succeed!", desc)
-    } else {
-        log::info!("{} failed: {:?}", desc, exit);
     }
 }
 
@@ -137,7 +128,7 @@ impl TestFixture<'_> {
                 "http://localhost:{}",
                 self.minio.get_host_port_ipv4(MINIO_DATA_PORT)
             )
-            .as_str(),
+                .as_str(),
         );
         builder.access_key_id("admin");
         builder.secret_access_key("password");
--- icelake/tests/utils/poetry.rs
+++ icelake/tests/utils/poetry.rs
@@ -1,4 +1,4 @@
-use crate::utils::{run_command, run_command_quiet};
+use crate::utils::run_command;
 use std::ffi::OsStr;
 use std::process::Command;
 
@@ -12,20 +12,13 @@ impl Poetry {
     pub fn new<S: ToString>(s: S) -> Self {
         let proj_dir = s.to_string();
 
-        // let mut cmd = Command::new(POETRY_CMD);
-        //
-        // cmd.args(["update", "--quiet"])
-        //     .current_dir(proj_dir.as_str());
-        //
-        // run_command_quiet(cmd, "poetry update");
-
         Self { proj_dir }
     }
 
     pub fn run_file(
         &self,
         file: &str,
-        other_args: impl IntoIterator<Item = impl AsRef<OsStr>>,
+        other_args: impl IntoIterator<Item=impl AsRef<OsStr>>,
         desc: impl ToString,
     ) {
         let mut cmd = Command::new(POETRY_CMD);
