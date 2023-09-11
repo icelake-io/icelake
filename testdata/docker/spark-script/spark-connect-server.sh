db31e7cef3dd15bee8a234caa77558a8cccd420f
Renjie Liu <liurenjie2008@gmail.com>
2023-09-11 10:07:26 +0800

    Use image in insertion test

--- testdata/docker/spark-script/spark-connect-server.sh
+++ testdata/docker/spark-script/spark-connect-server.sh
@@ -2,18 +2,11 @@
 
 set -ex
 
-#HTTP_PROXY=socks5://192.168.110.27:7890
-#HTTPS_PROXY=socks5://192.168.110.27:7890
-#  --conf spark.driver.extraJavaOptions="-Dhttps.proxyHost=192.168.110.27 -Dhttps.proxyPort=7890" \
+JARS=$(find /opt/spark/deps -type f -name "*.jar" | tr '\n' ':')
 
-ICEBERG_VERSION=1.3.1
-SPARK_VERSION=3.4.1
-
-PACKAGES="org.apache.iceberg:iceberg-spark-runtime-3.4_2.12:$ICEBERG_VERSION,org.apache.hadoop:hadoop-aws:3.3.2"
-PACKAGES="$PACKAGES,org.apache.spark:spark-connect_2.12:$SPARK_VERSION"
-
-/opt/spark/sbin/start-connect-server.sh --packages $PACKAGES \
+/opt/spark/sbin/start-connect-server.sh \
   --master local[3] \
+  --driver-class-path $JARS \
   --conf spark.driver.bindAddress=0.0.0.0 \
   --conf spark.sql.catalog.demo=org.apache.iceberg.spark.SparkCatalog \
   --conf spark.sql.catalog.demo.type=hadoop \
