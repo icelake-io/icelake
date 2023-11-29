51e90e6cf7e01915ed7daaca46c47daf21a3a422
ZENOTME <st810918843@gmail.com>
2023-11-29 18:18:59 +0800

    add base_writer in io_v2

---
+++ icelake/src/io_v2/base_writer/equality_delete_writer.rs
@@ -0,0 +1,147 @@
+//! This module provide `EqualityDeleteWriter`.
+use std::sync::Arc;
+
+use crate::{
+    io_v2::{
+        FileWriteResult, FileWriter, FileWriterBuilder, IcebergWriteResult, IcebergWriter,
+        IcebergWriterBuilder,
+    },
+    types::FieldProjector,
+    Error, ErrorKind, Result,
+};
+
+use arrow_array::RecordBatch;
+use arrow_schema::SchemaRef;
+use itertools::Itertools;
+
+#[derive(Clone)]
+pub struct EqualityDeleteWriterBuilder<B: FileWriterBuilder> {
+    inner: B,
+    equality_ids: Vec<usize>,
+}
+
+impl<B: FileWriterBuilder> EqualityDeleteWriterBuilder<B> {
+    pub fn new(inner: B, equality_ids: Vec<usize>) -> Self {
+        Self {
+            inner,
+            equality_ids,
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl<B: FileWriterBuilder> IcebergWriterBuilder for EqualityDeleteWriterBuilder<B> {
+    type R = EqualityDeleteWriter<B>;
+
+    async fn build(self, schema: &SchemaRef) -> Result<Self::R> {
+        let (projector, fields) = FieldProjector::new(schema.fields(), &self.equality_ids)?;
+        let delete_schema = Arc::new(arrow_schema::Schema::new(fields));
+        Ok(EqualityDeleteWriter {
+            inner_writer: self.inner.clone().build(&delete_schema).await?,
+            writer_builder: self.inner,
+            projector,
+            delete_schema,
+            equality_ids: self.equality_ids,
+        })
+    }
+}
+
+/// EqualityDeleteWriter is a writer that writes to a file in the equality delete format.
+pub struct EqualityDeleteWriter<B: FileWriterBuilder> {
+    writer_builder: B,
+    inner_writer: B::R,
+    projector: FieldProjector,
+    delete_schema: SchemaRef,
+    equality_ids: Vec<usize>,
+}
+
+#[async_trait::async_trait]
+impl<B: FileWriterBuilder> IcebergWriter for EqualityDeleteWriter<B> {
+    type R = <<B::R as FileWriter>::R as FileWriteResult>::R;
+    async fn write(&mut self, batch: RecordBatch) -> Result<()> {
+        let batch = RecordBatch::try_new(
+            self.delete_schema.clone(),
+            self.projector.project(batch.columns()),
+        )
+        .map_err(|err| Error::new(ErrorKind::IcebergDataInvalid, format!("{err}")))?;
+        self.inner_writer.write(&batch).await?;
+        Ok(())
+    }
+
+    async fn flush(&mut self) -> Result<Vec<Self::R>> {
+        let writer = std::mem::replace(
+            &mut self.inner_writer,
+            self.writer_builder
+                .clone()
+                .build(&self.delete_schema)
+                .await?,
+        );
+        let res = writer
+            .close()
+            .await?
+            .into_iter()
+            .map(|res| {
+                let mut res = res.to_iceberg_result();
+                res.set_content(crate::types::DataContentType::EqualityDeletes)
+                    .set_equality_ids(self.equality_ids.iter().map(|id| *id as i32).collect_vec());
+                res
+            })
+            .collect_vec();
+        Ok(res)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+
+    use crate::io_v2::{
+        test::{
+            create_arrow_schema, create_batch, create_location_generator, create_operator,
+            read_batch,
+        },
+        BaseFileWriterBuilder, EqualityDeleteWriterBuilder, IcebergWriteResult, IcebergWriter,
+        IcebergWriterBuilder, ParquetWriterBuilder,
+    };
+
+    #[tokio::test]
+    async fn test_equality_delete_writer() -> Result<(), anyhow::Error> {
+        // create writer
+        let op = create_operator();
+        let location_generator = create_location_generator();
+        let schema = create_arrow_schema(3);
+        let parquet_writer_builder = ParquetWriterBuilder::new(
+            op.clone(),
+            0,
+            Default::default(),
+            "/".to_string(),
+            Arc::new(location_generator),
+        );
+        let rolling_writer_builder = BaseFileWriterBuilder::new(None, parquet_writer_builder);
+        let mut equality_delete_writer =
+            EqualityDeleteWriterBuilder::new(rolling_writer_builder, vec![2, 3])
+                .build(&schema)
+                .await?;
+
+        // write 1024 * 3 column in 1 write
+        let to_write = create_batch(&schema, vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+        equality_delete_writer.write(to_write).await?;
+
+        // check output
+        let mut res = equality_delete_writer.flush().await.unwrap();
+        assert!(res.len() == 1);
+        res[0].set_partition(None);
+        let data_file = res[0].build().unwrap();
+        assert_eq!(data_file.equality_ids, Some(vec![2, 3]));
+        let batch = read_batch(&op, &data_file.file_path).await;
+        assert_eq!(
+            batch,
+            create_batch(
+                &Arc::new(schema.project(&[1, 2]).unwrap()),
+                vec![vec![4, 5, 6], vec![7, 8, 9]]
+            )
+        );
+
+        Ok(())
+    }
+}
---
+++ icelake/src/io_v2/base_writer/sort_writer.rs
@@ -0,0 +1,114 @@
+use arrow_array::RecordBatch;
+use arrow_schema::SchemaRef;
+use async_trait::async_trait;
+use itertools::Itertools;
+
+use crate::io_v2::{
+    FileWriteResult, FileWriter, FileWriterBuilder, IcebergWriter, IcebergWriterBuilder,
+};
+use crate::Result;
+
+pub(crate) struct SortWriterBuilder<I, B: FileWriterBuilder> {
+    inner: B,
+    sort_col_index: Vec<usize>,
+    cache_number: usize,
+    _marker: std::marker::PhantomData<I>,
+}
+
+impl<I, B: FileWriterBuilder> Clone for SortWriterBuilder<I, B> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+            sort_col_index: self.sort_col_index.clone(),
+            cache_number: self.cache_number,
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<I, B: FileWriterBuilder> SortWriterBuilder<I, B> {
+    pub fn new(inner: B, sort_col_index: Vec<usize>, cache_number: usize) -> Self {
+        Self {
+            inner,
+            sort_col_index,
+            cache_number,
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+#[async_trait]
+impl<I: Combinable, B: FileWriterBuilder> IcebergWriterBuilder for SortWriterBuilder<I, B> {
+    type R = SortWriter<I, B>;
+
+    async fn build(self, schema: &SchemaRef) -> Result<Self::R> {
+        Ok(SortWriter {
+            inner_builder: self.inner,
+            schema: schema.clone(),
+            cache: vec![],
+            current_cache_number: 0,
+            cache_number: self.cache_number,
+            _sort_col_index: self.sort_col_index,
+            result: vec![],
+        })
+    }
+}
+
+pub struct SortWriter<I: Combinable, B: FileWriterBuilder> {
+    inner_builder: B,
+    schema: SchemaRef,
+
+    cache: Vec<I>,
+    current_cache_number: usize,
+
+    cache_number: usize,
+    _sort_col_index: Vec<usize>,
+    result: Vec<<<B::R as FileWriter>::R as FileWriteResult>::R>,
+}
+
+impl<I: Combinable + Ord, B: FileWriterBuilder> SortWriter<I, B> {
+    async fn flush_by_ord(&mut self) -> Result<()> {
+        let mut new_writer = self.inner_builder.clone().build(&self.schema).await?;
+
+        self.current_cache_number = 0;
+        let mut cache = std::mem::take(&mut self.cache);
+        cache.sort();
+        let batch = Combinable::combine(cache);
+
+        // Write batch
+        new_writer.write(&batch).await?;
+        let res = new_writer
+            .close()
+            .await?
+            .into_iter()
+            .map(|res| res.to_iceberg_result())
+            .collect_vec();
+        self.result.extend(res);
+        Ok(())
+    }
+}
+
+#[async_trait::async_trait]
+impl<I: Combinable + Ord, B: FileWriterBuilder> IcebergWriter<I> for SortWriter<I, B> {
+    type R = <<B::R as FileWriter>::R as FileWriteResult>::R;
+    async fn write(&mut self, input: I) -> Result<()> {
+        self.current_cache_number += input.size();
+        self.cache.push(input);
+        if self.current_cache_number >= self.cache_number {
+            self.flush_by_ord().await?;
+        }
+        Ok(())
+    }
+
+    async fn flush(&mut self) -> Result<Vec<Self::R>> {
+        if !self.cache.is_empty() {
+            self.flush_by_ord().await?;
+        }
+        Ok(std::mem::take(&mut self.result))
+    }
+}
+
+pub trait Combinable: Sized + Send + 'static {
+    fn combine(vec: Vec<Self>) -> RecordBatch;
+    fn size(&self) -> usize;
+}
