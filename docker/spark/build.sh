304cfb2ce0b8d1962b64eeee9c2f717bb1c3e046
Renjie Liu <liurenjie2008@gmail.com>
2024-03-04 12:17:10 +0800

    spark: Add pg jdbc driver

--- docker/spark/build.sh
+++ docker/spark/build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-docker build -t icelake-spark .
+docker build --network host -t icelake-spark .
 docker tag icelake-spark:latest ghcr.io/icelake-io/icelake-spark:latest
 docker login ghcr.io
 docker push ghcr.io/icelake-io/icelake-spark:latest
