ebbee231398f9657a33648ef581916700438412b
Renjie Liu <liurenjie2008@gmail.com>
2024-03-04 12:31:24 +0800

    Add version

--- docker/spark/build.sh
+++ docker/spark/build.sh
@@ -3,6 +3,8 @@
 set -ex
 
 docker build --network host -t icelake-spark .
-docker tag icelake-spark:latest ghcr.io/icelake-io/icelake-spark:latest
+docker tag icelake-spark ghcr.io/icelake-io/icelake-spark:0.1
+docker tag icelake-spark ghcr.io/icelake-io/icelake-spark:latst
 docker login ghcr.io
+docker push ghcr.io/icelake-io/icelake-spark:0.1
 docker push ghcr.io/icelake-io/icelake-spark:latest
